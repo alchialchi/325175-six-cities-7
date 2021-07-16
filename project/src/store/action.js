62f98be2fda3e15e3685849ef5ef62f6bedab04c
Olga Lukina <obeskurajen@mail.ru>
2021-07-16 15:08:24 +0200

    Add auth and redirect

--- project/src/store/action.js
+++ project/src/store/action.js
@@ -1,8 +1,14 @@
+// import { adaptUserInfoToClient } from '../utils';
+
 export const ActionType = {
   ACTIVE_OFFER_ID: 'activeOffer',
   CHANGE_CITY: 'city/changeCity',
   SORT: 'sort/setSort',
   LOAD_OFFERS: 'data/loadOffers',
+  REQUIRE_AUTH: 'user/reduireAuth',
+  LOG_OUT: 'user/logout',
+  REDIRECT_TO_ROUTE: 'redirectToRoute',
+  SET_USER: 'user',
 };
 
 export const ActionCreator = {
@@ -21,5 +27,20 @@ export const ActionCreator = {
   loadOffers: (offers) => ({
     type: ActionType.LOAD_OFFERS,
     payload: offers,
+  }),
+  requireAuth: (status) => ({
+    type: ActionType.REQUIRE_AUTH,
+    payload: status,
+  }),
+  logout: () => ({
+    type: ActionType.LOG_OUT,
+  }),
+  redirectToRoute: (url) => ({
+    type: ActionType.REDIRECT_TO_ROUTE,
+    payload: url,
+  }),
+  setUser: (payload) => ({
+    type: ActionType.SET_USER,
+    payload,
   }),
 };
