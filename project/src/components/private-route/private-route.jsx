469b90917144646003fd4e18dfc032609b7c0ad0
Olga Lukina <obeskurajen@mail.ru>
2021-07-16 15:03:50 +0200

    Add PrivateRoute

---
+++ project/src/components/private-route/private-route.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { APP_ROUTES, AuthorizationStatus } from '../../const';
+
+function PrivateRoute({ render, path, exact, authorizationStatus }) {
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={(routeProps) => (
+        authorizationStatus === AuthorizationStatus.AUTH
+          ? render(routeProps)
+          : <Redirect to={APP_ROUTES.SIGN_IN} />
+      )}
+    />
+  );
+}
+
+PrivateRoute.propTypes = {
+  authorizationStatus: PropTypes.string.isRequired,
+  exact: PropTypes.bool.isRequired,
+  path: PropTypes.string.isRequired,
+  render: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  authorizationStatus: state.authorizationStatus,
+});
+
+export { PrivateRoute };
+export default connect(mapStateToProps)(PrivateRoute);
