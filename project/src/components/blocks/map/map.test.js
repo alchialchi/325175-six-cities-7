09fa64fa8beb89ecf202c9f0fb2f2277c977767b
Olga Lukina <obeskurajen@mail.ru>
2021-07-31 17:13:49 +0200

    Update map.test.js

--- project/src/components/blocks/map/map.test.js
+++ project/src/components/blocks/map/map.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Map from './Map';
+import Map from './map';
 import { offer } from '../../../mocks/offers';
 
 describe('Map', () => {
